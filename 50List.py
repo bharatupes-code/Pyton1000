e9644ebc21e71ca76a456b0e00586785fc214f96
bharatupes-code <bharatupes@gmail.com>
2026-03-16 19:18:45 +0530

    50List.py

---
+++ 50List.py
@@ -0,0 +1,12 @@
+num=[1,2,3]+[4,5,6]#concatenation
+print(num)
+
+ch=['N!']*4
+print(ch)#repetation
+
+L = ['spam', 'Spam', 'SPAM!']
+L[1] = 'eggs' # Index assignment
+print(L)
+
+L[0:2] = ['eat', 'more'] # Slice assignment: delete+insert
+print(L)
